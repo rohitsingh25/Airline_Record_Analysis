205689461329416c19cf0c2bdc0364a0115b107f
rohit <rsy708943@gmail.com>
2023-10-29 01:11:20 +0530

    Bar Graph

--- Backend/main.py
+++ Backend/main.py
@@ -1,5 +1,5 @@
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import numpy as np 
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
@@ -11,7 +11,7 @@ df=pd.read_csv("Airline Dataset Updated.csv", parse_dates=["Departure Date"])
 df.head(11)
 df.info()
 df.nunique()
-df.isnull().sum().sum() #Checking if Null value is present
+df.isnull().sum().sum() 
 df['Gender']=df['Gender'].astype('category')
 df['Gender'].unique()
 df['Age']=df['Age'].astype(int)
@@ -22,4 +22,9 @@ for index, column in enumerate(columns):
 count_1 = df['Gender'].value_counts()
 count_1
 fig = px.pie(count_1, values=count_1.values, names=count_1.index,title= "Distribution of passengers accross Genders")
+fig.show()
+count_2 = df['Country Name'].value_counts()
+fig = px.bar(count_2.head(10), x=count_2.head(10).index, y=count_2.head(10).values,
+             title='Top 10 Airport Countries with Most Passengers',
+             labels={'x': 'Country', 'y': 'Passenger Count'})
 fig.show()
