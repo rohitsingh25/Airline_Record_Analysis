616f7e4447ffd16b0761e116716e5e65e582b5d0
rohit <rsy708943@gmail.com>
2023-11-01 15:31:03 +0530

    main.py updated

--- Backend/main.py
+++ Backend/main.py
@@ -1,28 +1,32 @@
-import numpy as np 
-import pandas as pd
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import os
-for dirname, _, filenames in os.walk('/kaggle/input'):
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
+
 df=pd.read_csv("Airline Dataset Updated.csv", parse_dates=["Departure Date"])
 df.head(11)
 df.info()
 df.nunique()
-df.isnull().sum().sum() 
+df.isnull().sum().sum() #Checking if Null value is present
+
 df['Gender']=df['Gender'].astype('category')
 df['Gender'].unique()
 df['Age']=df['Age'].astype(int)
+
 _, ax = plt.subplots(figsize=(12, 12), ncols=2, nrows=2,  )
 columns = [key for key, value in df.nunique().to_dict().items() if value < 10]
+
 for index, column in enumerate(columns):
     df[column].value_counts().plot(ax=ax.ravel()[index] ,kind='bar', legend=True)
+
 count_1 = df['Gender'].value_counts()
 count_1
+
 fig = px.pie(count_1, values=count_1.values, names=count_1.index,title= "Distribution of passengers accross Genders")
 fig.show()
+
 count_2 = df['Country Name'].value_counts()
 fig = px.bar(count_2.head(10), x=count_2.head(10).index, y=count_2.head(10).values,
              title='Top 10 Airport Countries with Most Passengers',
